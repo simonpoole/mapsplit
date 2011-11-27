4ef18ed35f4270b0995f552d592fbcd960d8fcd7
Peter Barth <osm-mapsplit@won2.de>
2011-11-27 18:22:49 +0100

    initial commit

---
+++ src/HeapMap.java
@@ -0,0 +1,294 @@
+/*
+ * OSMSplitter - A simple but fast tile splitter for large OSM data
+ * 
+ * Written in 2011 by Peda (osm-splitter@won2.de)
+ * 
+ * To the extent possible under law, the author(s) have dedicated all copyright and 
+ * related and neighboring rights to this software to the public domain worldwide. 
+ * This software is distributed without any warranty.
+ * 
+ * You should have received a copy of the CC0 Public Domain Dedication along with 
+ * this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+/**
+ * An HashMap in Heap memory, optimized for size to use in OSM
+ */
+@SuppressWarnings("unchecked")
+public class HeapMap implements OsmMap {
+
+	// used on keys
+	private static final int BUCKET_FULL_SHIFT = 63;
+	private static final long BUCKET_FULL_MASK = 1l << BUCKET_FULL_SHIFT;
+	private static final long KEY_MASK = ~BUCKET_FULL_MASK;
+
+	// used on values
+	static final int TILE_X_SHIFT = 51;
+	static final long TILE_X_MASK = 8191l << TILE_X_SHIFT;
+	static final int TILE_Y_SHIFT = 38;
+	static final long TILE_Y_MASK = 8191l << TILE_Y_SHIFT;
+	static final int NEIGHBOUR_SHIFT = 36;
+	static final long NEIGHBOUR_MASK = 3l << NEIGHBOUR_SHIFT;
+	static final int TILE_EXT_SHIFT = 35;
+	static final long TILE_EXT_MASK = 1l << TILE_EXT_SHIFT;		
+	static final long TILE_MARKER_MASK = 0x7FFFFFFFFl;
+
+	private int size;
+	private long[] keys;
+	private long[] values;
+
+	private int extendedBuckets = 0;
+	private Set<Integer>[] extendedSet = new Set[1000];
+
+
+	public HeapMap(int size) {
+		this.size = size;
+		keys = new long[size];
+		values = new long[size];
+	}
+
+	public int tileX(long value) {
+		return (int) ((value & TILE_X_MASK) >>> TILE_X_SHIFT);
+	}
+
+	public int tileY(long value) {
+		return (int) ((value & TILE_Y_MASK) >>> TILE_Y_SHIFT);
+	}
+
+	public int neighbour(long value) {
+		return (int) ((value & NEIGHBOUR_MASK) >> NEIGHBOUR_SHIFT);
+	}
+
+	/* (non-Javadoc)
+	 * @see OsmMap#put(long, int, int, int)
+	 */
+	public void put(long key, int tileX, int tileY, int neighbours) {
+		int count = 0;
+
+		int bucket = (int) (key % size);
+		long value = ((long)tileX) << TILE_X_SHIFT | 
+		((long)tileY) << TILE_Y_SHIFT |
+		((long) neighbours) << NEIGHBOUR_SHIFT;
+
+		while (true) {
+			if (values[bucket] == 0) {
+				keys[bucket] = key;
+				values[bucket] = value;
+				return;
+			} 
+			if (count == 0) {
+				// mark bucket as "overflow bucket" 
+				keys[bucket] |= BUCKET_FULL_MASK;
+			}
+			bucket++; count++;
+
+			if (count >= size)
+				throw new Error("HashMap filled up, increase the (static) size!");
+		}
+	}
+
+	private int getBucket(long key) {
+		int count = 0;
+
+		int bucket = (int) (key % size);
+
+		while (true) {
+			if ((values[bucket] != 0) && 
+					((keys[bucket] & KEY_MASK) == key)) {
+				return bucket;
+			}
+			if (count == 0) {
+				// we didn't have an overflow so the value is not stored yet
+				if (keys[bucket] >= 0)
+					return -1;
+			}
+			bucket++; count++;				
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see OsmMap#get(long)
+	 */
+	public long get(long key) {
+		int bucket = getBucket(key);
+
+		if (bucket == -1)
+			return 0;
+
+		return values[bucket];
+	}
+
+	private void appendNeighbours(int index, List<Long> tiles) {
+		Set<Integer> set = extendedSet[index];
+
+		for (long l : tiles) {
+			int tx = tileX(l);
+			int ty = tileY(l);
+
+			set.add(tx << 13 | ty);
+		}
+	}
+
+	private void extendToNeighbourSet(int bucket, List<Long> tiles) {
+
+		long val = values[bucket];
+
+		if ((val & TILE_MARKER_MASK) != 0) {
+
+			// add current stuff to tiles list
+			List<Integer> tmpList = parseMarker(val);
+			for (int i : tmpList) {
+				long tx = i >> 13;
+			long ty = i & 8191;
+			long temp = tx << TILE_X_SHIFT | ty << TILE_Y_SHIFT;
+
+			tiles.add(temp);
+			}
+
+			// delete old marker from val
+			val &= ~TILE_MARKER_MASK;
+		}
+
+		int cur = extendedBuckets++;
+
+		// if we don't have enough sets, increase the array...
+		if (cur >= extendedSet.length) {
+			Set<Integer>[] tmp = new Set[2*extendedSet.length];
+			System.arraycopy(extendedSet, 0, tmp, 0, extendedSet.length);
+			extendedSet = tmp;
+		}
+
+		val |= 1l << TILE_EXT_SHIFT;
+		val |= (long) cur;
+		values[bucket] = val;
+
+		extendedSet[cur] = new TreeSet<Integer>();
+
+		appendNeighbours(cur, tiles);
+	}
+
+	/* (non-Javadoc)
+	 * @see OsmMap#update(long, java.util.List)
+	 */
+	public void update(long key, List<Long> tiles) {
+
+		int bucket = getBucket(key);
+		long val = values[bucket];
+		int tx = tileX(val);
+		int ty = tileY(val);
+
+		// neighbour list is already too large so we use the "large store"
+		if ((val & TILE_EXT_MASK) != 0) {
+
+			int idx = (int) (val & TILE_MARKER_MASK);
+			appendNeighbours(idx, tiles);
+
+			return;
+		}
+
+		// now we use the 35 reserved bits for the tiles list..
+		boolean extend = false;
+		for (long tile : tiles) {
+
+			int tmpX = tileX(tile);
+			int tmpY = tileY(tile);
+
+			if (tmpX == tx && tmpY == ty)
+				continue;
+
+			int dx = tmpX - tx + 2;
+			int dy = tmpY - ty + 2;
+
+			int idx = dy * 6 + dx;
+			if (idx >= 14) idx--;
+
+			if (dx < 0 || dy < 0 || dx > 5 || dy > 5) {
+				// .. damn, not enough space for "small store" 
+				//  -> use "large store" instead
+				extend = true;
+				break;
+			} else {
+				val |= 1l << idx;
+			}
+		}
+
+		if (extend) {
+			extendToNeighbourSet(bucket, tiles);
+		} else {		
+			values[bucket] = val;
+		}
+	}
+
+	private List<Integer> parseMarker(long value) {
+		List<Integer> result = new ArrayList<Integer>();
+		int tx = tileX(value);
+		int ty = tileY(value);
+
+		for (int i = 0; i < 35; i++) {
+			// if bit is not set, continue.. 
+			if (((value >> i) & 1) == 0)
+				continue;
+
+			int v = i >= 14? i+1 : i;
+
+			int tmpX = v % 6 - 2;
+			int tmpY = v / 6 - 2;
+
+			result.add((tx + tmpX) << 13 | (ty + tmpY));
+		}
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see OsmMap#getAllTiles(long)
+	 */
+	public List<Integer> getAllTiles(long key) {
+
+		List<Integer> result;
+		long value = get(key);
+
+		if (value == 0) {
+			return null;
+		}
+
+		int tx = tileX(value);
+		int ty = tileY(value);
+		int neighbour = neighbour(value);
+
+		if ((value & TILE_EXT_MASK) != 0) {
+			int idx = (int) (value & TILE_MARKER_MASK);
+			Set<Integer> set = extendedSet[idx];
+
+			result = new ArrayList<Integer>(set);
+			result.add(tx << 13 | ty);
+
+			// TODO neighbourhood treatment...
+
+			return result;				
+		}
+
+		result = parseMarker(value);
+
+		result.add(tx << 13 | ty);
+		// TODO neighbourhood treatment...
+
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see OsmMap#getLoad()
+	 */
+	public double getLoad() {
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (values[i] != 0) count++;
+		}
+		return ((double) count) / ((double) size);
+	}
+}
---
+++ src/OSMSplitter.java
@@ -0,0 +1,531 @@
+/*
+ * OSMSplitter - A simple but fast tile splitter for large OSM data
+ * 
+ * Written in 2011 by Peda (osm-splitter@won2.de)
+ * 
+ * To the extent possible under law, the author(s) have dedicated all copyright and 
+ * related and neighboring rights to this software to the public domain worldwide. 
+ * This software is distributed without any warranty.
+ * 
+ * You should have received a copy of the CC0 Public Domain Dedication along with 
+ * this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
+import org.openstreetmap.osmosis.core.container.v0_6.NodeContainer;
+import org.openstreetmap.osmosis.core.container.v0_6.RelationContainer;
+import org.openstreetmap.osmosis.core.container.v0_6.WayContainer;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
+import org.openstreetmap.osmosis.core.domain.v0_6.Relation;
+import org.openstreetmap.osmosis.core.domain.v0_6.RelationMember;
+import org.openstreetmap.osmosis.core.domain.v0_6.Way;
+import org.openstreetmap.osmosis.core.domain.v0_6.WayNode;
+import org.openstreetmap.osmosis.core.task.v0_6.RunnableSource;
+import org.openstreetmap.osmosis.core.task.v0_6.Sink;
+
+import crosby.binary.file.BlockOutputStream;
+import crosby.binary.osmosis.OsmosisReader;
+import crosby.binary.osmosis.OsmosisSerializer;
+
+
+public class OSMSplitter {
+
+	/*
+	 * the zoom-level at which we render our tiles
+	 * Attention: Code is not generic enough to chance this value without
+	 * further code changes! ;)
+	 */	
+	private static final int ZOOM = 13;
+	
+	/*
+	 * the default sizes for the hash maps: should be a factor 2-4 of nodes 
+	 * in the pbf you want to read
+	 */  
+	private static final int NODE_MAP_SIZE     = 60000000;
+	private static final int WAY_MAP_SIZE      = 10000000;	
+	private static final int RELATION_MAP_SIZE =  2500000;
+	
+	// all data after this appointment date is considered new or modified
+	private Date appointmentDate;
+	
+	private Date latestDate = new Date(0);
+	
+	// the input file we're going to split
+	private File input;
+	
+	// internal store to check if reading the file worked
+	private boolean complete = false;
+
+	// the hashmap for all nodes in the osm map
+	private OsmMap nmap;
+
+	// the hashmap for all ways in the osm map
+	private OsmMap wmap;
+
+	// the hashmap for all relations in the osm map
+	private OsmMap rmap;
+
+	// a bitset telling the algorithm which tiles need to be rerendered
+	private BitSet modifiedTiles = new BitSet();
+	
+	// the serializer (OSM writers) for any modified tile
+	private Map<Integer, OsmosisSerializer> outFiles;
+	
+
+	
+	
+	public OSMSplitter(Date appointmentDate, File file) {
+		this.input = file;
+		this.appointmentDate = appointmentDate;
+		nmap = new HeapMap(NODE_MAP_SIZE);
+		wmap = new HeapMap(WAY_MAP_SIZE);
+		rmap = new HeapMap(RELATION_MAP_SIZE);
+	}
+
+	public static double tile2lon(int x) {
+		return (x / Math.pow(2.0, ZOOM)) * 360.0 - 180.0;
+	}
+	
+	public static double tile2lat(int y) {
+		double n = Math.PI - 2.0 * Math.PI * y / Math.pow(2, ZOOM);
+	    return (180.0 / Math.PI * Math.atan(0.5 * (Math.pow(Math.E, n) - Math.pow(Math.E, -n))));
+	}
+	
+	public static int lon2tileX(double lon) {
+		return (int) Math.floor((lon + 180.0) / 360.0 * Math.pow(2.0,ZOOM));
+	}
+	
+	public static int lat2tileY(double lat) { 
+		return (int) Math.floor((1.0 - Math.log(Math.tan(lat * Math.PI / 180.0) + 1.0/Math.cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0 * Math.pow(2.0,ZOOM));
+	}
+	
+	private void addNodeToMap(Node n, double lat, double lon) {
+		int tileX = lon2tileX(lon);
+		int tileY = lat2tileY(lat);
+
+		// TODO: check and add border
+		// TODO: mark tile border to be rerendered
+		
+		// mark current tile to be rerendered
+		if (n.getTimestamp().after(appointmentDate))
+			modifiedTiles.set(tileX << 13 | tileY);
+		
+		// mark the latest changes made to this map
+		if (n.getTimestamp().after(latestDate))
+			latestDate = n.getTimestamp();
+		
+		nmap.put(n.getId(), tileX, tileY, OsmMap.NEIGHBOURS_NONE);
+	}
+	
+	private void addWayToMap(Way way) {
+		
+		boolean modified = way.getTimestamp().after(appointmentDate);
+		List<Long> tileList = new ArrayList<Long>();
+		
+		// mark the latest changes made to this map
+		if (way.getTimestamp().after(latestDate))
+			latestDate = way.getTimestamp();
+		
+		for (WayNode wayNode : way.getWayNodes()) {
+			// get tileNrs for given node
+			long tile = nmap.get(wayNode.getNodeId());
+			
+			// mark tiles as modified
+			if (modified) {
+				// TODO: missing border treatment...
+				int tx = nmap.tileX(tile);
+				int ty = nmap.tileY(tile);
+				modifiedTiles.set(tx << 13 | ty);
+			}
+			
+			tileList.add(tile);
+		}
+		
+		// bootstrap a tilepos for the way
+		long id = way.getWayNodes().get(0).getNodeId();
+		long val = nmap.get(id);
+		int tx = nmap.tileX(val);
+		int ty = nmap.tileY(val);
+		
+		// put way into map with a "random" base tile
+		wmap.put(way.getId(), tx, ty, OsmMap.NEIGHBOURS_NONE);
+		// update map so that the way knows which tiles it belongs to
+		wmap.update(way.getId(), tileList);
+		
+		for (WayNode wayNode : way.getWayNodes()) {
+			
+			// update map so that the node knows about any additional
+			// tile it has to be stored in
+			nmap.update(wayNode.getNodeId(), tileList);
+		}
+	}
+	
+	private void addRelationToMap(Relation r) {
+	
+		boolean modified = r.getTimestamp().after(appointmentDate);
+		List<Long> tileList = new ArrayList<Long>();
+		
+		if (r.getTimestamp().after(latestDate))
+			latestDate = r.getTimestamp();
+		
+		
+		for (RelationMember m : r.getMembers()) {
+		
+			switch (m.getMemberType()) {
+			case Node:
+				long tile = nmap.get(m.getMemberId());
+
+				// mark tiles as modified
+				if (modified) {
+					int tx = nmap.tileX(tile);
+					int ty = nmap.tileY(tile);
+					modifiedTiles.set(tx << 13 | ty);
+				}
+				
+				tileList.add(tile);
+				break;
+				
+			case Way:
+				List<Integer> list = wmap.getAllTiles(m.getMemberId());
+				
+				if (modified) {
+					for (Integer i : list)
+						modifiedTiles.set(i);
+				}
+			
+				// FIXME: TODO: add ways tiles to tileList..
+				//tileList.addAll(list);
+				break;
+			}
+			
+			long val = tileList.get(0);
+			int tx = rmap.tileX(val);
+			int ty = rmap.tileY(val);
+			
+			// put relation into map with a "random" base tile
+			rmap.put(r.getId(), tx, ty, OsmMap.NEIGHBOURS_NONE);
+			// update map so that the relation knows in which tiles it is needed
+			rmap.update(r.getId(), tileList);
+	
+			
+			// TODO I'm not sure yet if all nodes and all ways belonging to the
+			//      given relation need to be in every tile that this relation
+			//      is in. I.e. do we need a "complete-relation" setting as we
+			//      do it for "complete-ways"??			
+		}
+	}
+	
+	public void setup() throws IOException {
+		
+		RunnableSource reader =	new OsmosisReader(new FileInputStream(input));
+		reader.setSink(new Sink() {
+			@Override
+			public void release() { /* nothing to be done */ }			
+			@Override
+			public void complete() { complete = true; }
+			@Override
+			public void process(EntityContainer ec) {
+				if (ec instanceof NodeContainer) {
+					
+					Node n = ((NodeContainer) ec).getEntity();
+					addNodeToMap(n, n.getLatitude(), n.getLongitude());
+					
+				} else if (ec instanceof WayContainer) {
+					
+					Way w = ((WayContainer) ec).getEntity();
+					addWayToMap(w);
+					
+				} else if (ec instanceof RelationContainer) {
+					
+					Relation r = ((RelationContainer) ec).getEntity();
+					addRelationToMap(r);
+					
+				} else {
+					System.err.println("Unknown Element while reading");
+					System.err.println(ec.toString());
+					System.err.println(ec.getEntity().toString());
+				}
+			}
+		});
+		
+		Thread readerThread = new Thread(reader);
+		readerThread.start();		
+		while (readerThread.isAlive()) {
+			try {
+				readerThread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (!complete)
+			throw new IOException("Could not read file fully");
+	}
+	
+	
+	public void store(String basename) throws IOException {
+		
+		complete = false;
+		outFiles = new HashMap<Integer, OsmosisSerializer>();
+		
+		// Setup out-files...
+		int idx = 0; 
+		while (true) {
+			idx = modifiedTiles.nextSetBit(idx+1);
+			if (idx == -1)
+				break;
+			
+			if (outFiles.get(idx) == null) {
+
+				String file = basename + (idx >> 13) + "_" + (idx & 8191) + ".pbf"; 
+				//System.out.println(file);
+			
+				OsmosisSerializer serializer =
+					new OsmosisSerializer(new BlockOutputStream(new FileOutputStream(file)));
+
+				serializer.setUseDense(true);
+				outFiles.put(idx, serializer);
+			}
+		}
+	
+		// Now start writing output...
+		
+		RunnableSource reader =	new OsmosisReader(new FileInputStream(input));
+		reader.setSink(new Sink() {
+			@Override
+			public void release() { 
+				// nothing to be done 
+			}			
+			@Override
+			public void complete() { complete = true; }
+			@Override
+			public void process(EntityContainer ec) {
+				long id = ec.getEntity().getId();
+				
+				List<Integer> tiles;
+				
+				if (ec instanceof NodeContainer)
+					tiles = nmap.getAllTiles(id);
+				else if (ec instanceof WayContainer)
+					tiles = wmap.getAllTiles(id);
+				else if (ec instanceof RelationContainer) {
+					tiles = rmap.getAllTiles(id);
+					// TODO reactivate once we have relations...
+					return;
+				} else {
+					System.err.println("Unknown Element while reading");
+					System.err.println(ec.toString());
+					System.err.println(ec.getEntity().toString());
+					return;
+				}
+				
+				for (int i : tiles) {
+					if (modifiedTiles.get(i)) {
+						outFiles.get(i).process(ec);
+					}
+				}
+			}
+		});
+		
+		Thread readerThread = new Thread(reader);
+		readerThread.start();
+		while (readerThread.isAlive()) {
+			try {
+				readerThread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (!complete)
+			throw new IOException("Could not fully read file in second run");
+		
+		
+		// Finish and close files...
+		for (Entry<Integer, OsmosisSerializer> entry : outFiles.entrySet()) {
+			OsmosisSerializer ser = entry.getValue();
+	
+			ser.complete();
+			ser.flush();
+			ser.close();
+		}
+	}
+	
+	
+	
+	/*
+	 * Command line handling and main function
+	 */
+	
+	
+	private static Date run(String inputFile, 
+			  		        String outputBase,
+			 			    Date appointmentDate,
+						    boolean verbose,
+						    boolean timing) throws Exception {
+
+		long startup = System.currentTimeMillis();
+
+		OSMSplitter split = new OSMSplitter(appointmentDate, new File(inputFile));
+		
+		long time = System.currentTimeMillis();
+		split.setup();
+		time = System.currentTimeMillis() - time;
+		
+		int modified = split.modifiedTiles.cardinality();
+		
+		if (timing)
+			System.out.println("Initial reading and datastructure setup took " + time + "ms");
+		if (verbose)
+			System.out.println("We have " + modified + " modified tiles to store.");
+		
+		time = System.currentTimeMillis();
+		split.store(outputBase);
+		time = System.currentTimeMillis() - time;
+		if (timing) {
+			System.out.println("Saving " + modified + " tiles took " + time + "ms");
+			long overall = System.currentTimeMillis() - startup;
+			System.out.print("\nOverall runtime: " + overall + "ms");
+			System.out.println(" == " + (overall / 1000 / 60) + "min");
+		}
+		
+		if (verbose) {
+			System.out.println("\nHashmaps load:");
+			System.out.println("Nodes    : " + split.nmap.getLoad());
+			System.out.println("Ways     : " + split.wmap.getLoad());
+			System.out.println("Relations: " + split.rmap.getLoad());
+		}
+		
+		return split.latestDate;
+	}
+	
+	
+	private static void help() {
+		System.out.println("Usage: OSMSplitter [options] <infile> <output base>");
+		System.out.println("OSMSplitter loads infile and stores any tile that got changed during the last 2 days in a tile file.\n");
+		System.out.println("infile: A tile file in pbf format");
+		System.out.println("output base: this is the base name of all tiles that will be written. The filename will be preceeded with the tilenumber at Zoom 13\n");
+
+		System.out.println("Options:");
+		System.out.println("  -h, --help         this help");
+		System.out.println("  -v, --verbose      additional informational output");
+		System.out.println("  -t, --timing       output timing information");
+		System.out.println("  -d, --date=file    file containing the date since when tiles are being considered to have changed");
+		System.out.println("                     after the split the latest change in infile is going to be stored in file");
+		System.out.println("  -s, --size=n,w,r   the size for the node-, way- and relation maps to use (should be at least twice the number of IDs)");
+	}
+	
+
+	
+	public static void main(String[] args) throws Exception {
+		
+		int idx, mandatory = 0;
+		Date appointmentDate;
+		String inputFile = null;
+		String outputBase = null;
+		boolean verbose = false;
+		boolean timing = false;
+		String dateFile = null;
+		int[] mapSizes = new int[]{NODE_MAP_SIZE, WAY_MAP_SIZE, RELATION_MAP_SIZE};
+		
+		// Simple argument parser..
+		for (int i = 0; i < args.length; i++) {
+			
+			if (args[i].startsWith("-")) {
+				if (args[i].startsWith("--")) args[i] = args[i].substring(1);
+				switch (args[i].charAt(1)) {
+				case 'h':
+					help();
+					System.exit(0);
+				case 'v':
+					verbose = true;
+					break;
+				case 't':
+					timing = true;
+					break;
+				case 'd':
+					idx = args[i].indexOf('=');
+					if (idx == -1) {
+						System.err.println("Supply a date file");
+						help();
+						System.exit(1);
+					}
+					dateFile = args[i].substring(idx+1);
+					break;
+				case 's':
+					idx = args[i].indexOf('=');
+					if (idx == -1) {
+						System.err.println("Missing values for map sizes");
+						help();
+						System.exit(2);
+					}
+					String tmp = args[i].substring(idx+1);
+					String[] vals = tmp.split(",");
+					for (int j = 0; j < 3; j++)
+						mapSizes[j] = Integer.valueOf(vals[j]);
+					break;
+				}
+			} else if (mandatory++ == 0) {
+				inputFile = args[i];
+			} else if (mandatory++ == 1) {
+				outputBase = args[i];
+			} else {
+				System.err.println("Ignoring extra argument \"" + args[i] + "\"");
+			}
+		}
+		
+		if (mandatory < 2) {
+			System.err.println("Please supply an input file and output basename");
+			help();
+			System.exit(3);
+		}
+
+		
+		// Date-setup as fall-back option
+		DateFormat df = DateFormat.getDateTimeInstance();
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		c.add(Calendar.DAY_OF_MONTH, -3);
+		appointmentDate = c.getTime();
+
+		
+		if (dateFile == null && verbose)
+				System.out.println("No datefile given. Checking for changes in the last three days.");		
+		else {
+			
+			DataInputStream dis = new DataInputStream(new FileInputStream(dateFile));
+			String line = dis.readLine();
+			
+			if (line != null)
+				appointmentDate = df.parse(line);
+			
+			dis.close();
+		}
+		
+		// Actually run the splitter... 
+		Date latest = run(inputFile, outputBase, appointmentDate, verbose, timing);
+		
+		if (verbose)
+			System.out.println("Last changes to the map had been done on " + df.format(latest));
+		if (dateFile != null) {
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(dateFile));
+			dos.writeChars(df.format(latest));
+		}
+	}	
+}
